8075d9f9e4deb29626d0fc8974e1141cfaaa6184
oleg3790 <oleg.krysko@outlook.com>
2018-11-22 19:02:22 -0600

    Add login component

---
+++ src/Oversr/ClientApp/src/components/Login.js
@@ -0,0 +1,46 @@
+﻿import React from 'react'
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    onUsernameChange = () => {
+
+    }
+
+    onPasswordChange = () => {
+
+    }
+
+    onSubmit = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <form className="col-6 offset-3" onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label for="username"></label>
+                            <input id="username" className="form-control" onChange={this.onUsernameChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label for="password"></label>
+                            <input id="password" className="form-control" onChange={this.onPasswordChange}></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary"></button>                        
+                    </form>
+                </div>                
+            </div>            
+        );
+    }
+}
