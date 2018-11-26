e527976f177b9148636519eb6150cc8000c5ff1f
oleg3790 <oleg.krysko@outlook.com>
2018-11-25 20:09:19 -0600

    Add login based off db

--- src/Oversr/Model/User.cs
+++ src/Oversr/Model/User.cs
@@ -1,12 +1,17 @@
-﻿namespace Oversr.Model
+﻿using System;
+
+namespace Oversr.Model
 {
     public class User
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
         public string Token { get; set; }
     }
 }
