73f9325a5db51bf6ce0cf267b30fd319706c6e6f
oleg3790 <oleg.krysko@outlook.com>
2018-11-28 22:45:33 -0600

    Add sample inventory component

--- src/Oversr/ClientApp/src/commons/NavigationTabs.js
+++ src/Oversr/ClientApp/src/commons/NavigationTabs.js
@@ -4,7 +4,7 @@ export default function NavigationTabs(props) {
     return (
         <ul className="nav nav-tabs">
             {props.navItems.map(x =>
-                    <li key={x.id} data-id={x.id} className="nav-item" onClick={props.onSelectionChange}>
+                    <li key={x.id} data-id={x.id} className={x.selected ? "nav-item nav-item-active" : "nav-item"} onClick={props.onSelectionChange}>
                         <span className={x.selected ? "nav-link active" : "nav-link"}>{x.value}</span>
                     </li>
                 )}
