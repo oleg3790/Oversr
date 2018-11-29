fbd0bf97f85f9d05ebc8d43f081f392c1cd36d89
oleg3790 <oleg.krysko@outlook.com>
2018-11-28 21:03:28 -0600

    Add Inventory component and tab logic

---
+++ src/Oversr/ClientApp/src/components/Inventory.js
@@ -0,0 +1,38 @@
+﻿import React, { Component } from 'react';
+import NavigationTabs from '../commons/NavigationTabs';
+import { ObjectAssignmentHelpers } from '../commons/ObjectAssignmentHelpers';
+
+export default class Inventory extends Component {
+    constructor(props) {
+        super(props);
+        this.handleTabSelectionChange = this.handleTabSelectionChange.bind(this);
+        this.state = {
+            types: [
+                {
+                    id: 0,
+                    value: 'Sample Inventory',
+                    selected: true
+                },
+                {
+                    id: 1,
+                    value: 'Order Inventory',
+                    selected: false
+                }
+            ]
+        }
+    }
+
+    handleTabSelectionChange(e) {
+        const id = e.currentTarget.dataset.id;
+        const changedObj = ObjectAssignmentHelpers.changeSelectionById(this.state.types, id);
+        this.setState({ types: changedObj });
+    }
+
+    render() {
+        return (
+            <div className="mt-2">
+                <NavigationTabs navItems={this.state.types} onSelectionChange={this.handleTabSelectionChange}/>
+            </div>
+        );
+    }
+}
