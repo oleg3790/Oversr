cdc2c9f3044ae32aa0fc9eef431cabe3067e702b
oleg3790 <oleg.krysko@outlook.com>
2018-12-05 21:05:54 -0600

    Cleanup

--- src/Oversr/ClientApp/src/App.js
+++ src/Oversr/ClientApp/src/App.js
@@ -6,15 +6,14 @@ import Login from './components/Login';
 import { Layout } from './components/Layout';
 
 export default class App extends Component {
-  render() {
-    
-    return (
-        <Layout>
-            <Switch>
-                <PrivateRoute exact path="/" component={Home}/>
-                <Route path="/login" component={Login}/>
-            </Switch>            
-        </Layout>
-    );
-  }
+    render() {    
+        return (
+            <Layout>
+                <Switch>
+                    <PrivateRoute exact path="/" component={Home}/>
+                    <Route path="/login" component={Login}/>
+                </Switch>            
+            </Layout>
+        );
+    }
 }
