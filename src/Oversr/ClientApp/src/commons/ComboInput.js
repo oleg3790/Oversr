aa5a517ead0205e4091b4e98a04a2c2bd4eaf798
oleg3790 <oleg.krysko@outlook.com>
2018-12-12 00:09:32 -0600

    Finish ComboInput base logic

--- src/Oversr/ClientApp/src/commons/ComboInput.js
+++ src/Oversr/ClientApp/src/commons/ComboInput.js
@@ -29,20 +29,20 @@ export default class ComboInput extends Component {
 
     render() {
         return (
-            <div className="input-group">
-                <div className="input-group-prepend">
-                    <label className="input-group-text">{this.props.labelText}</label>
+            <div className="input-group row no-gutters">
+                <div className="input-group-prepend col-3">
+                    <label className="input-group-text d-inline-block text-right w-100">{this.props.labelText}</label>
                 </div>
                 {this.state.isInputActive 
                     ? (
-                        <div className="row no-gutters">
-                            <input className="form-control col-9"></input>   
-                            <div className="col-1">
-                                <FontAwesomeIcon icon={faTimes} size="2x" className="text-danger"/>                                
-                            </div>   
-                            <div className="col-1">
-                                <FontAwesomeIcon icon={faCheck} size="2x" className="text-success"/>
-                            </div>                    
+                        <div className="col-9 row no-gutters">
+                            <input className="form-control pl-2 col-8" placeholder="Enter a new value"></input>  
+                            <button className="btn btn-success col-2 ml-3">
+                                <FontAwesomeIcon icon={faCheck} size="lg" className="text-light"/> 
+                            </button>  
+                            <button className="btn btn-danger col-1 ml-2" onClick={this.toggleInputSelection}>
+                                <FontAwesomeIcon icon={faTimes} size="lg" className="text-light"/>
+                            </button>                                                                    
                         </div>)
                     : (
                         <select className="form-control" onChange={this.handleSelectionChange}>
