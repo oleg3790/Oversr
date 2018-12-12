5e8955c5a8414fb1227910d65178543e0e2b6784
Oleg-Krysko <Oleg.Krysko@coxautoinc.com>
2018-12-12 16:35:58 -0600

    Add ComboInput button titles and save function

--- src/Oversr/ClientApp/src/commons/ComboInput.js
+++ src/Oversr/ClientApp/src/commons/ComboInput.js
@@ -4,9 +4,10 @@ import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 export default class ComboInput extends Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.toggleInputSelection = this.toggleInputSelection.bind(this);
         this.handleSelectionChange = this.handleSelectionChange.bind(this);
+        this.handleNewValueSave = this.handleNewValueSave.bind(this);
         this.state = {
             isInputActive: false
         }
@@ -17,7 +18,7 @@ export default class ComboInput extends Component {
 
         if (val === "Add new value") {
             this.toggleInputSelection();
-        } 
+        }
         else {
             this.props.onSelectionChange(val);
         }
@@ -27,32 +28,36 @@ export default class ComboInput extends Component {
         this.setState({ isInputActive: !this.state.isInputActive });
     }
 
+    handleNewValueSave() {
+
+    }
+
     render() {
         return (
             <div className="input-group row no-gutters">
                 <div className="input-group-prepend col-3">
                     <label className="input-group-text d-inline-block text-right w-100">{this.props.labelText}</label>
                 </div>
-                {this.state.isInputActive 
+                {this.state.isInputActive
                     ? (
                         <div className="col-9 row no-gutters">
-                            <input className="form-control pl-2 col-8" placeholder="Enter a new value"></input>  
-                            <button className="btn btn-success col-2 ml-3">
-                                <FontAwesomeIcon icon={faCheck} size="lg" className="text-light"/> 
-                            </button>  
-                            <button className="btn btn-danger col-1 ml-2" onClick={this.toggleInputSelection}>
-                                <FontAwesomeIcon icon={faTimes} size="lg" className="text-light"/>
-                            </button>                                                                    
+                            <input className="form-control pl-2 col-8" placeholder="Enter a new value"></input>
+                            <button className="btn btn-success col-2 ml-3" title="Save" onClick={this.handleNewValueSave}>
+                                <FontAwesomeIcon icon={faCheck} size="lg" className="text-light" />
+                            </button>
+                            <button className="btn btn-danger col-1 ml-2" onClick={this.toggleInputSelection} title="Cancel">
+                                <FontAwesomeIcon icon={faTimes} size="lg" className="text-light" />
+                            </button>
                         </div>)
                     : (
                         <select className="form-control" onChange={this.handleSelectionChange}>
                             <option></option>
-                            {this.props.selectionItems.map((x,i) =>
+                            {this.props.selectionItems.map((x, i) =>
                                 <option key={i}>{x.value}</option>
                             )}
                             <option className="combo-input">Add new value</option>
                         </select>)}
-            </div>             
+            </div>
         );
     }
 }
