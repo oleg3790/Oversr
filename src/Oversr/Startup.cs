4406faecbff903d7fd5718a194ace6a491ca7140
oleg3790 <oleg.krysko@outlook.com>
2018-12-13 21:29:08 -0600

    Add get designer service and controller

--- src/Oversr/Startup.cs
+++ src/Oversr/Startup.cs
@@ -51,7 +51,8 @@ namespace Oversr
             {
                 configuration.RootPath = "ClientApp/build";
             });
-            services.AddScoped<IUserService, UserService>();
+            services.AddSingleton<IUserService, UserService>();
+            services.AddSingleton<IInventoryService, InventoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
