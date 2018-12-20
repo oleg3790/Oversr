3f9886bdaa2df4a9c55bfe62bb05eadc6606dcea
oleg3790 <oleg.krysko@outlook.com>
2018-12-20 12:18:14 -0600

    Add loader to Sample Inventory view

--- src/Oversr/ClientApp/src/commons/BusyOverlay.js
+++ src/Oversr/ClientApp/src/commons/BusyOverlay.js
@@ -4,7 +4,7 @@ import '../styles/BusyLoader.css';
 
 export default function BusyOverlay(props) {
     return (
-        <div className="load-overlay">
+        <div className="load-overlay" style={props.isAbsolute ? { position: 'absolute' } : { position: 'relative' }}>
             <div className="load-position">
                 <RollingLoader/>
             </div>                                
