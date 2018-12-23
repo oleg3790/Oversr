dfd226d5ef6af277c142132a7f5859cae8e22d55
oleg3790 <oleg.krysko@outlook.com>
2018-12-22 19:35:07 -0600

    Add validation to Designers controller

---
+++ src/Oversr/Model/ViewModel/NewDesignerVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oversr.Model.ViewModel
+{
+    public class NewDesignerVM
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
