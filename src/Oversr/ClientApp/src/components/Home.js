9714f0b6bfa768be7f5de747f587620e7e3c4b21
oleg3790 <oleg.krysko@outlook.com>
2018-12-25 14:28:02 -0600

    Add designers route and other front-end stuff

--- src/Oversr/ClientApp/src/components/Home.js
+++ src/Oversr/ClientApp/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Inventory from './Inventory';
 
 export class Home extends Component {
     render() {
         return (
             <div>
-                <Inventory/>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/designers">Designers</Link>
             </div>
         );
     }
