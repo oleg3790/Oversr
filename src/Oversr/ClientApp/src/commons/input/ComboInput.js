40a2817a8fc9051ff0ae83ca6a705ccca4b636b4
Oleg-Krysko <Oleg.Krysko@coxautoinc.com>
2018-12-27 16:30:30 -0600

    Font-end style work

--- src/Oversr/ClientApp/src/commons/input/ComboInput.js
+++ src/Oversr/ClientApp/src/commons/input/ComboInput.js
@@ -1,88 +1,18 @@
-﻿import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
+﻿import React from 'react';
 
-export default class ComboInput extends Component {
-    constructor(props) {
-        super(props);
-        this.toggleInputSelection = this.toggleInputSelection.bind(this);
-        this.handleNewValueTextChange = this.handleNewValueTextChange.bind(this);
-        this.handleSelectionChange = this.handleSelectionChange.bind(this);
-        this.handleNewValueSave = this.handleNewValueSave.bind(this);
-        this.resetError = this.resetError.bind(this);
-        this.state = {
-            isInputActive: false,
-            newValueText: null,
-            error: null
-        };
-    }
-
-    handleSelectionChange(e) {
-        this.resetError();
-        const val = e.target.value;
-
-        if (val === "Add new value") {
-            this.toggleInputSelection();
-        }
-        else {
-            this.props.onSelectionChange(e);
-        }
-    }
-
-    toggleInputSelection() {
-        this.setState({ isInputActive: !this.state.isInputActive });
-        this.resetError();
-    }
-
-    handleNewValueTextChange(e) {
-        this.setState({ newValueText: e.target.value });
-        this.resetError();
-    }
-
-    handleNewValueSave(e) {
-        e.preventDefault();
-        this.resetError();
-
-        if (this.state.newValueText) {
-            this.props.onNewValueSave(this.state.newValueText);
-            this.setState({ isInputActive: false });
-        }
-        else {
-            this.setState({ error: 'Value cannot be empty' });
-        }
-    }
-
-    resetError() {
-        this.setState({ error: null });
-    }
-
-    render() {
-        return (
-            <div className="input-group row no-gutters">
-                <div className="input-group-prepend">
-                    <label className={"input-group-text " + this.props.labelClass}>{this.props.labelText}</label>
-                </div>
-                {this.state.isInputActive
-                    ? (
-                        <div className="col-9 row no-gutters">
-                            <input className="form-control pl-2 col-8" placeholder="Enter a new value" onChange={this.handleNewValueTextChange}></input>
-                            <button className="btn btn-success col-2 ml-3" title="Save" onClick={this.handleNewValueSave}>
-                                <FontAwesomeIcon icon={faCheck} size="lg" className="text-light" />
-                            </button>
-                            <button className="btn btn-danger col-1 ml-2" onClick={this.toggleInputSelection} title="Cancel">
-                                <FontAwesomeIcon icon={faTimes} size="lg" className="text-light" />
-                            </button>
-                        </div>)
-                    : (
-                        <select className="form-control" id={this.props.fieldId} onChange={this.handleSelectionChange}>
-                            <option></option>
-                            {this.props.selectionItems && this.props.selectionItems.map(x =>
-                                <option key={x.id}>{x.name}</option>
-                            )}
-                            <option className="combo-input">Add new value</option>
-                        </select>)}
-                {this.state.error && <small className="col-5 offset-3 text-danger">{this.state.error}</small>}
+export default function ComboInput(props) {
+    return (
+        <div className="input-group row no-gutters">
+            <div className="input-group-prepend">
+                <label className="input-group-text" 
+                    style={{ display: 'block', textAlign: 'right', width: props.labelWidth ? props.labelWidth : '100px' }}>{props.label}</label>
             </div>
-        );
-    }
+            <select className="form-control" id={props.fieldId} onChange={props.onSelectionChange}>
+                <option></option>
+                {props.selectionItems && props.selectionItems.map(x =>
+                    <option key={x.id}>{x.name}</option>
+                )}
+            </select>
+        </div>
+    );    
 }
